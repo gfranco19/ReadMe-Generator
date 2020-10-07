fda2716b4a9bc76101be2262f24a02cb4c9a846c
Giovanny Franco <gfranco19@live.com>
2020-10-07 12:37:32 -0700

    added const varible and tested first input

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquire = require ("inquirer");
+const fs = require ("fs");
+const util = require ("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+inquire.prompt([
+    {
+        type: "input",
+        message:"What is your project title?",
+        name:"projectTitle"
+    }
+])
